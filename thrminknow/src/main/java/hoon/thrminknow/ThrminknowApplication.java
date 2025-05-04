39d802bf8cafa344c81e982d423c7069acf374fe
Hoon <totohoon02@gmail.com>
2025-05-04 10:49:10 +0900

    프로젝트 init, swagger setting

---
+++ thrminknow/src/main/java/hoon/thrminknow/ThrminknowApplication.java
@@ -0,0 +1,13 @@
+package hoon.thrminknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrminknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrminknowApplication.class, args);
+	}
+
+}
---
+++ thrminknow/src/test/java/hoon/thrminknow/ThrminknowApplicationTests.java
@@ -0,0 +1,13 @@
+package hoon.thrminknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrminknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
